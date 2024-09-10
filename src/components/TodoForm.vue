2aefcf177c8360d0e6578ef77181f5a793425321
HosseinrezaAk <hrakbari77@gmail.com>
2024-09-10 23:50:43 +0330

    add files

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/todoList'
+import func from '../../vue-temp/vue-editor-bridge'
+
+const todo = ref('')
+
+const store = useTodoListStore()
+
+function addItemAndClear(item){
+    if(item.length === 0){
+        return;
+    }
+    store.addTodo(item)
+    todo.value = ''
+}
+
+</script>
+<template>
+    <div>
+        <form @submit.prevent="addItemAndClear">
+            <input v-model="todo" type="text">
+            <button>Add</button>
+        </form>
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
